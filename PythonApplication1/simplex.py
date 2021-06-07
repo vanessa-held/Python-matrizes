6f7f255f005ae0f2acb75b01d676d020592971f7
Vanessa Held <nessa.hofstatter@gmail.com>
2021-06-07 20:14:02 -0300

    Seprando melhor o codigo

--- PythonApplication1/simplex.py
+++ PythonApplication1/simplex.py
@@ -97,66 +97,81 @@ def informar_termos_independentes(n):
     return matriz
 
 def exemplo_manual():
+    # m = int(input("Quantas variaveis de decisão tem o problema:"))
+    # C = informar_funcao_objetivo(m)  # [[1, 4, 2]]
+    # A, n = informar_matriz_restricoes(m)
+    # D = informar_termos_independentes(n)
+
     m = int(input("Quantas variaveis de decisão tem o problema:"))
     C = informar_funcao_objetivo(m)  #
     A, n = informar_matriz_restricoes(m)
-    D = informar_termos_independentes(n)
+    b = informar_termos_independentes(n)
 
-    return m, C, A, n, D
+    return m, C, A, n, b
 
 def exemplo_1():
     C =  [[1, 4, 2]]
     m = len(C[0])
     A = [[2, 2, 0], [1, 0, 3], [1, 1, 2]]
     n = len(A)
-    D = [[20, 15, 40]]
-    return m, C, A, n, D
+    b = [[20, 15, 40]]
+    return m, C, A, n, b
 
 def exemplo_2():
     C = [[4, 1]]
     m = len(C[0])
     A = [[9, 1], [3, 1]]
     n = len(A)
-    D = [[18, 12]]
-    return m, C, A, n, D
+    b = [[18, 12]]
+    return m, C, A, n, b
 
-if __name__ == '__main__':
-    # m = int(input("Quantas variaveis de decisão tem o problema:"))
-    # C = informar_funcao_objetivo(m)  # [[1, 4, 2]]
-    # A, n = informar_matriz_restricoes(m)
-    # D = informar_termos_independentes(n)
 
-    m, C, A, n, D = exemplo_1()
-    #simplex
-    b = F.transposta(D)
-    X = F.transposta([[f'x{i}' for i in range(1, m + 1)]])
+def print_variaveis_na_base(Xb, Rb):
+    print(f"Variaveis da base:")
+    for l in range(len(Rb)):
+        for c in range(len(Rb[l])):
+            print(f"{Xb[l][c]} = {Rb[l][c]}")
+
+
+def print_solucao_otima(Z):
+    print(f"Solucao otima: {Z[0][0]}")
+
+
+def print_variaveis_fora_base(X):
+    print(f"Variaveis fora da base:")
+    for l in range(len(X)):
+        for c in range(len(X[l])):
+            print(f"{X[l][c]} = 0")
+
+
+def simplex(m, C, A, n, b):
+    b = F.transposta(b)
+    X = F.transposta([[f'X{i}' for i in range(1, m + 1)]])
     Xb = F.transposta([[f'S{i}' for i in range(1, n + 1)]])
-
     B = F.gerar_identidade(n)
-
-    Cb = [[0]*n]
-
+    Cb = [[0] * n]
     # Definir variaveis de base
-
     # Definir quem entra na base
     C1 = F.mult_escalar(C, -1)
     while verificar_valores_negativos(C1):
-        l, coluna_entrada, valor_entrada = def_entra_base(C1)
-        entra_na_base = F.transposta(X)[l][coluna_entrada]
+        l, entra, valor_entrada = def_entra_base(C1)
+        entra_na_base = F.transposta(X)[l][entra]
 
         # Definir quem sai da base
-        linha_saida, val = def_sai_base(A, b, coluna_entrada)
-        Xb[linha_saida][0] = entra_na_base
+        sai, val = def_sai_base(A, b, entra)
+
+        X[entra][0] = Xb[sai][0]
+        Xb[sai][0] = entra_na_base
 
         B = trocar_coluna(
             # Bug
             B,
-            linha_saida,
+            sai,
             A,
-            coluna_entrada
+            entra
         )
         Bi = F.matriz_inversa(B)
-        Cb[0][linha_saida] = valor_entrada
+        Cb[0][sai] = valor_entrada
 
         matriz_opera = F.matriz_mult(Cb, Bi)
         matriz_opera = F.matriz_mult(matriz_opera, A)
@@ -164,8 +179,20 @@ if __name__ == '__main__':
         C1 = matriz_opera
 
         matriz_opera2 = F.matriz_mult(Cb, Bi)
-
     Rb = F.matriz_mult(Bi, b)
     Z = F.matriz_mult(Cb, Bi)
     Z = F.matriz_mult(Z, b)
-    print(Z)
+
+    return X, Xb, Rb, Z
+
+
+if __name__ == '__main__':
+    m, C, A, n, b = exemplo_1()
+    X, Xb, Rb, Z = simplex(m, C, A, n, b)
+
+    print_solucao_otima(Z)
+    print_variaveis_na_base(Xb, Rb)
+    print_variaveis_fora_base(X)
+
+
+
